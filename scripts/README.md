20f1412c37160084aa6cc385f0f3b01614021113
FenryrMKIII <fenryrmkiii@fenryrmkiii>
2024-05-19 22:12:46 +0200

    feat: corrected on/off, implemented buttons reactions & logic, prepared scaffolding for add/multiply/substract/divide

--- scripts/README.md
+++ scripts/README.md
@@ -1,4 +1,14 @@
 # Questions 
 - How should one implement a scientific calculator e.g. able to perform "complex" calculations like processing an expression with multiple operators, parentheses, ... ?
 - How one should implement the "memory" feature in a calculator? Is a global variable a good idea?
-- 
+- How to efficiently and beautifully assign functions to classes and ids? Maybe go with dictionnary (class vs id, function to attach) and define a function to assign all classes/ids with identified function ? Maybe some frameworks have some shortcuts for that given it's something you always have to do ?
+- how to avoid warnings double declaration for "standard" double declarations like when you create a dummy variable to do somethign ? e.g. 
+
+// assign operate function
+  let button = document.getElementById('equals');
+...
+  };
+let button = document.getElementById('add');
+...
+};
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,55 +1,138 @@
-let memory = undefined;
-let current_value="0";
+let first_value=undefined;
+let second_value=undefined
+let operator = undefined
 let power_state = false
 
 // Select all buttons with the 'numbers' class
-let buttons = document.getElementsByClassName('numbers');
-
-// Loop through each button
+let buttons = document.getElementsByClassName('numbers_button');
 for (let i = 0; i < buttons.length; i++) {
-  // Add an 'onclick' event listener to each button
   buttons[i].onclick = function() {
-    // Call the read_input function with the button's text as argument
-    read_input(this.textContent);
-  };
+    let number_entered = this.textContent;
+    if (power_state) {
+    update_state(number_entered);
+    refresh_display(this.textContent);
+  } else {
+     }
+  }
 }
 
-let button = document.getElementById('power_on');
+function assignFunctionToButton(classOrId, label, func) {
+  if (classOrId === 'class') {
+    let button = document.getElementsByClassName(label);
+    button.onclick = function () {
+      func();
+    }
+  }
+  else {
+    let button = document.getElementById(label);
+    button.onclick = function() {
+      func();
+    }
+    }}
 
-// Assign the onclick function
-button.onclick = function() {
-  power_on();
+// assign math operators function
+let operators_dict = {
+  'add': add,
+  'subtract': substract,
+  'multiply': multiply,
+  'divide': divide
 };
 
-function power_on() {
+for (let key in operators_dict) {
+  let button = document.getElementById(key);
+  button.onclick = function() {
+    operator = operators_dict[key]()
+  }
+}
+
+// assign operate function
+let button = document.getElementById('equals');
+button.onclick = function() {
+  operate();
+};
+
+function get_display() {
+  return document.getElementById('display');
+}
+
+function refresh_display() {
+  let display = get_display();
+  if (!power_state) {
+    return
+  }
+    if (operator === undefined) {
+      if (first_value === undefined) {
+        display.value = '0';
+      } else {
+        display.value = first_value;
+      }
+    }
+
+    else {
+      if (second_value === undefined) {
+        display.value = '0';
+      } else {
+        display.value = second_value;
+      }
+    }
+  }
+
+function power_on(){
   power_state = !power_state;
+
   if (power_state) {
-  let inputElement = document.getElementById('display');
-  inputElement.value = current_value;}
+    console.log('Calculator is on');
+    refresh_display();
+  }
   else {
-    let inputElement = document.getElementById('display');
-    inputElement.value = "";
+    console.log('Calculator is off');
+    first_value = undefined;
+    second_value = undefined;
+    operator = undefined;
+    get_display().value = ''
   }}
 
-function read_input(a) {
-  if (current_value === "0") {
-    current_value = a;
-  } else {
-    current_value = current_value + a;
+function update_state(a) {
+  if (operator === undefined) {
+    if (first_value === undefined) {
+      first_value = a;
+    } else {
+      first_value = first_value + a;
+    }
+  }
+  else if (second_value === undefined) {
+    second_value = a;
+  }
+  else {
+    second_value = second_value + a;
   }
 }
 
-function add(memory, b) {
-  let result;
-
-  if (memory !== undefined) {
-    result = memory + b;
-  } else {
-    result = "memory is not defined"
-  }
-  return result;
+function add() {
+  return first_value + second_value;
 }
 
-function assign_memory(a) {
-  memory = a;
+function multiply() {
+  return first_value + second_value;
 }
+
+function substract() {
+  return first_value + second_value;
+}
+
+function divide() {
+  return first_value + second_value;
+}
+
+function operate() {
+  let operations_dict = {'add': add(), 'multiply': multiply(), 'substract': substract(), 'divide': divide()}
+  operations_dict[operator](first_value, second_value)
+}
+
+assignFunctionToButton('id', 'power_on', power_on);
+assignFunctionToButton('id', 'equals', operate);
+assignFunctionToButton('id', 'equals', add);
+
+console.log(first_value)
+console.log(second_value)
+console.log(operator)
